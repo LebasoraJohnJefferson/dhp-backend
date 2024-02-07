792d0a2dd87226f66cefc814960d69671fe63a17
Eris <johnjefferson.lebasora@evsu.edu.ph>
2024-02-07 22:15:20 +0800

    refactor: duplicate route

--- routes/api.php
+++ routes/api.php
@@ -51,37 +51,34 @@ Route::middleware('auth:sanctum')->group(function () {
 
     Route::resource('/infant',InfantController::class);
     Route::resource('/recover_files',RecoverFilesController::class);
+    
+    Route::resource('/famityProfile',FamiltyProfileController::class)->only(['destroy','store','index','show']);
+    Route::resource('/file',FileController::class);
+    Route::resource('/famityProfileMembers',FamilyProfileMemberController::class)->only(['destroy','store','index','show']);
+    Route::resource('/baranggay',BaranggayController::class)->only(['destroy','store','index','show']);
+    Route::resource('/dashboard',DashboardContoller::class)->only(['index','show']);
+    
+
     // Admin routes and middleware where roles is being check if admin
     Route::prefix('/admin')->middleware(AdminMiddleware::class)->group(function () {
         Route::patch('/personnel/status',[PersonnelController::class,'change_personnel_status']);
-        Route::resource('/famityProfile',FamiltyProfileController::class)->only(['destroy','store','index','show']);
         Route::get('/profileFamiltyAnalytics',[AnalyticsFamiltyProfileController::class,'FPAnalyic']);
         Route::get('/profileInfantAnlytics',[AnalyticsFamiltyProfileController::class,'InfantAnalyic']);
-        Route::get('get_all_invited_province/{event_id}',[EventInvatationController::class,'invited_province']);
-        Route::resource('/file',FileController::class);
-        Route::resource('/famityProfileMembers',FamilyProfileMemberController::class)->only(['destroy','store','index','show']);
+        Route::get('/get_all_invited_province/{event_id}',[EventInvatationController::class,'invited_province']);
 
-        Route::resource('/baranggay',BaranggayController::class)->only(['destroy','store','index','show']);
         Route::resource('/logs',LogsController::class)->only(['index','show']);
         Route::resource('/profile', AdminController::class);
         Route::resource('/event',EventController::class);
         Route::resource('/recover_personnel',RecoverPersonnelContoller::class);
         Route::resource('/personnel',PersonnelController::class);
-        Route::resource('/dashboard',DashboardContoller::class);
         Route::resource('/event_invitation',EventInvatationController::class);
-        Route::resource('/personnel/importExcel',ImportPersonnelContoller::class)
-            ->only(['store']);
+        Route::resource('/personnel/importExcel',ImportPersonnelContoller::class)->only(['store']);
     });
 
     // Personnel routes and middleware where roles is being check if personnel
     Route::prefix('/personnel')->middleware(PersonnelMiddleware::class)->group(function () {
-        Route::resource('/famityProfile',FamiltyProfileController::class)->only(['destroy','store','index','show']);
-        Route::resource('/baranggay',BaranggayController::class)->only(['destroy','store','index','show']);
-        Route::resource('/famityProfileMembers',FamilyProfileMemberController::class)->only(['destroy','store','index','show']);
         Route::resource('/', UserController::class)->only(['index'])->only(['index']);
-        Route::resource('/dashboard',DashboardContoller::class)->only(['index']);
-        Route::resource('/event',EventController::class)->only(['index']);
-        Route::resource('/file',FileController::class)->only(['index','destroy','store']);
+        Route::resource('/personnelEvent',EventController::class)->only(['index']);
     });
 
 
