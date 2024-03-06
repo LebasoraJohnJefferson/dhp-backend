681f0790830a75789407abbabf08a6e2276a40c5
Eris <johnjefferson.lebasora@evsu.edu.ph>
2024-03-07 06:57:30 +0800

    small fix #

--- app/Traits/SmsSender.php
+++ app/Traits/SmsSender.php
@@ -1,24 +1,28 @@
 <?php
 
 namespace App\Traits;
-use Illuminate\Support\Facades\Log;
+use Pnlinh\InfobipSms\Facades\InfobipSms;
+
 
 trait SmsSender{
     protected function sendSms($msg, $phone_number){
 
-        $basic  = new \Vonage\Client\Credentials\Basic(env("VONAGE_API_KEY"), env("VONAGE_API_SECRET"));
-        $client = new \Vonage\Client($basic);
+        // $basic  = new \Vonage\Client\Credentials\Basic(env("VONAGE_API_KEY"), env("VONAGE_API_SECRET"));
+        // $client = new \Vonage\Client($basic);
 
-        $response = $client->sms()->send(
-            new \Vonage\SMS\Message\SMS($phone_number, 'RHU BARUGO', $msg)
-        );
+        // $response = $client->sms()->send(
+        //     new \Vonage\SMS\Message\SMS($phone_number, 'RHU BARUGO', $msg)
+        // );
 
-        $message = $response->current();
+        // $message = $response->current();
 
-        if ($message->getStatus() == 0) {
-            error_log("The message was sent successfully\n");
-        } else {
-            error_log("The message failed with status: " . $message->getStatus() . "\n");
-        }
+        // if ($message->getStatus() == 0) {
+        //     error_log("The message was sent successfully\n");
+        // } else {
+        //     error_log("The message failed with status: " . $message->getStatus() . "\n");
+        // }
+        $response = InfobipSms::send('+639772671851',$msg);
+        
+        error_log(json_encode($response));
     }
 }
